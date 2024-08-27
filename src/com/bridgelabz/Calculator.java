454862b9035056b8f5f2a5dda1543bdbd28a9b49
pushpakdeore <deorepushpakk@gmail.com>
2024-08-27 17:15:26 +0530

    Added:Subtraction fun

--- src/com/bridgelabz/Calculator.java
+++ src/com/bridgelabz/Calculator.java
@@ -4,7 +4,12 @@ public class Calculator {
     public static void main(String[] args) {
         System.out.println("wellcome to Calculator ");
         int a=10,b=20,c;
+        //uc1_addition
         c =a+b;
         System.out.println("Addition is c"+c);
+        //uc2_sub
+        c =a-b;
+        System.out.println("Addition is c"+c);
+
     }
 }
